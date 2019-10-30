e1d1e2a15a5334b2d1eaaaad95444fe166f311bd
Joel Myers <joeldmyers@gmail.com>
2019-10-29 21:06:45 -0400

    Modifying Prettier config, setting up airbnb eslint settings, refactoring Header to be functional

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">
+    <nav>
+      <ul className="navbar-nav bd-navbar-nav flex-row">
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            Home
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
